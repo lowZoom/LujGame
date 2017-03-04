96df64a26171fc69ccfc0f27cc93f9246c4377b4
lowZoom <david_lu_st@163.com>
2017-03-04 18:53:42 +0800

    添加机器人工程

--- robot/src/main/java/lujgame/robot/boot/RobotBoot.java
+++ robot/src/main/java/lujgame/robot/boot/RobotBoot.java
@@ -1,11 +1,26 @@
 package lujgame.robot.boot;
 
+import akka.actor.ActorSystem;
+import com.typesafe.config.Config;
+import lujgame.core.file.DataFileReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RobotBoot {
 
-  public void boot(String[] args) {
-    System.out.println("机器人程序启动...");
+  @Autowired
+  public RobotBoot(
+      DataFileReader dataFileReader) {
+    _dataFileReader = dataFileReader;
   }
+
+  public void boot(String[] args) {
+    Config systemCfg = _dataFileReader.readConfig("akka.conf");
+    ActorSystem system = ActorSystem.create("Robot", systemCfg);
+
+
+  }
+
+  private final DataFileReader _dataFileReader;
 }
